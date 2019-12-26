152e4a04e3faa068627c484ce76870db7a20bf3b
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-12-26 14:50:04 +0100

    Add license headers

--- include/xcpp/xmime.hpp
+++ include/xcpp/xmime.hpp
@@ -1,5 +1,6 @@
 /***************************************************************************
-* Copyright (c) 2016, Johan Mabille, Loic Gouarin and Sylvain Corlay       *
+* Copyright (c) 2016, Johan Mabille, Loic Gouarin, Sylvain Corlay          *
+* Copyright (c) 2016, QuantStack                                           *
 *                                                                          *
 * Distributed under the terms of the BSD 3-Clause License.                 *
 *                                                                          *
--- test/test_stream.cpp
+++ test/test_stream.cpp
@@ -1,3 +1,12 @@
+/***********************************************************************************
+* Copyright (c) 2016, Johan Mabille, Loic Gouarin, Sylvain Corlay, Wolf Vollprecht *
+* Copyright (c) 2016, QuantStack                                                   *
+*                                                                                  *
+* Distributed under the terms of the BSD 3-Clause License.                         *
+*                                                                                  *
+* The full license is in the file LICENSE, distributed with this software.         *
+************************************************************************************/
+
 #include "gtest/gtest.h"
 
 #include <functional>
