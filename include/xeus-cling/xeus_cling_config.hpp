f2df30e80ba2ca88ed8850231372e31ad5ec3ea6
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-08-29 00:08:47 +0200

    Release 0.10.0

--- include/xeus-cling/xeus_cling_config.hpp
+++ include/xeus-cling/xeus_cling_config.hpp
@@ -12,7 +12,7 @@
 
 // Project version
 #define XEUS_CLING_VERSION_MAJOR 0
-#define XEUS_CLING_VERSION_MINOR 9
+#define XEUS_CLING_VERSION_MINOR 10
 #define XEUS_CLING_VERSION_PATCH 0
 
 // Composing the version string from major, minor and patch
