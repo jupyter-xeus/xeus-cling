fbc0f539f1088e653095bd047276c068fee57e56
Johan Mabille <johan.mabille@gmail.com>
2022-10-24 22:15:02 +0200

    Migrated to doctest

--- test/test_stream.cpp
+++ test/test_stream.cpp
@@ -7,7 +7,7 @@
 * The full license is in the file LICENSE, distributed with this software.         *
 ************************************************************************************/
 
-#include "gtest/gtest.h"
+#include "doctest/doctest.h"
 
 #include <functional>
 #include <iostream>
@@ -23,13 +23,16 @@ void callback(std::string value, std::list<std::string>& outputs)
     outputs.push_back(value);
 }
 
-TEST(stream, output)
+TEST_SUITE("stream")
 {
-    std::list<std::string> outputs;
-    xcpp::xoutput_buffer buffer(std::bind(callback, _1, std::ref(outputs)));
-    auto cout_strbuf = std::cout.rdbuf();
-    std::cout.rdbuf(&buffer);
-    std::cout << "Some output" << std::endl;
-    EXPECT_EQ(outputs.front(), "Some output\n");
-    std::cout.rdbuf(cout_strbuf);
+    TEST_CASE("output")
+    {
+        std::list<std::string> outputs;
+        xcpp::xoutput_buffer buffer(std::bind(callback, _1, std::ref(outputs)));
+        auto cout_strbuf = std::cout.rdbuf();
+        std::cout.rdbuf(&buffer);
+        std::cout << "Some output" << std::endl;
+        REQUIRE_EQ(outputs.front(), "Some output\n");
+        std::cout.rdbuf(cout_strbuf);
+    }
 }
