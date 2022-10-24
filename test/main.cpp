cac5fb740201c7ebb652869d957f1293e789b3f0
Johan Mabille <johan.mabille@gmail.com>
2022-10-24 22:15:02 +0200

    Migrated to doctest

--- test/main.cpp
+++ test/main.cpp
@@ -7,10 +7,5 @@
 * The full license is in the file LICENSE, distributed with this software.         *
 ************************************************************************************/
 
-#include <gtest/gtest.h>
-
-int main(int argc, char* argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest/doctest.h"
